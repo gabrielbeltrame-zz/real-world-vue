a561d1af3f61021077eb22ed3934dabe09e81f1a
Gabriel Beltrame Silva <silvagabrielbeltrame@gmail.com>
2020-07-13 21:55:30 -0300

    inicio das rotas

---
+++ src/views/EventShow.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Showing event #{{ id }}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      default: null
+    }
+  }
+}
+</script>
+
+<style></style>
